bc808b84b891c61c53badedf47bc4212022e45be
bharath-gp <gpbharath@yahoo.in>
2017-02-27 17:14:35 +0530

    Fix the rebalance after failover test where the failed over nodes were not removed from the servers to consider.

--- pytests/failover/AutoFailoverTests.py
+++ pytests/failover/AutoFailoverTests.py
@@ -64,9 +64,6 @@ class AutoFailoverTests(AutoFailoverBaseTest):
         servers = deepcopy(self.servers)
         servers = [server for server in servers if server not in
                    self.server_to_fail]
-        self.log.info(servers)
-        self.log.info(self.servers)
-        self.log.info(self.server_to_fail)
         rebalance_success = self.cluster.rebalance(servers,
                                                    self.servers_to_add,
                                                    self.servers_to_remove)
