9b8764bb407b2509c855d0d26caad0ed43d8efb6
bharath-gp <gpbharath@yahoo.in>
2017-03-10 18:52:58 +0530

    Start timer after failure injection

--- lib/tasks/task.py
+++ lib/tasks/task.py
@@ -4644,7 +4644,6 @@ class AutoFailoverNodesFailureTask(Task):
                     self.set_exception(Exception("Reset of autofailover "
                                                  "count failed"))
         self.current_failure_node = self.servers_to_fail[self.itr]
-        self.start_time = time.time()
         if self.failure_type == "enable_firewall":
             self._enable_firewall(self.current_failure_node)
         elif self.failure_type == "disable_firewall":
@@ -4667,6 +4666,7 @@ class AutoFailoverNodesFailureTask(Task):
                                                    self.servers_to_fail[
                                                        self.itr + 1])
             self.itr += 1
+        self.start_time = time.time()
         self.itr += 1
 
     def _enable_firewall(self, node):
