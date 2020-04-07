3f5a18bdf0c1a81c484d54148f22eb3ddb8eb5be
bharathgp <bharath.gp@couchbase.com>
2020-04-07 15:54:23 +0530

    Fixed iptables_modifier to take in host names as input

--- scripts/iptables_modifier.py
+++ scripts/iptables_modifier.py
@@ -1,29 +1,28 @@
 from optparse import OptionParser
 import paramiko
 
-def main(operation, user, password):
-    ssh1 = paramiko.SSHClient()
-    ssh1.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh1.connect(hostname="172.23.104.150", username=user, password=password)
-
-    ssh2 = paramiko.SSHClient()
-    ssh2.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh2.connect(hostname="172.23.104.136", username=user, password=password)
+def main(hosts, operation, user, password):
+    hosts = hosts.split(',')
+    ssh_connections = []
+    for host in hosts:
+        _host = host.trim('"')
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh.connect(hostname=_host, username=user, password=password)
+        ssh_connections.append(ssh)
 
     if operation == "close":
         print("Closing ports")
         command_1 = "/sbin/iptables -A INPUT -p tcp -i eth0 --dport 1000:65535 -j REJECT"
         command_2 = "/sbin/iptables -A OUTPUT -p tcp -o eth0 --sport 1000:65535 -j REJECT"
-
-        ssh1.exec_command(command_1)
-        ssh1.exec_command(command_2)
-        ssh2.exec_command(command_1)
-        ssh2.exec_command(command_2)
+        for ssh in ssh_connections:
+            ssh.exec_command(command_1)
+            ssh.exec_command(command_2)
     elif operation == "open":
         print("Opening ports")
         command = "/sbin/iptables -F"
-        ssh1.exec_command(command)
-        ssh2.exec_command(command)
+        for ssh in ssh_connections:
+            ssh.exec_command(command)
     else:
         print("Unknown operation!")
         exit(1)
@@ -34,7 +33,9 @@ if __name__ == "__main__":
     parser.add_option('-o','--operation', dest='operation')
     parser.add_option('-u','--user', dest='user')
     parser.add_option('-p','--password', dest='password')
+    parser.add_option('-i', '--hosts', dest='hosts')
 
     options, args = parser.parse_args()
 
-    main(options.operation, options.user, options.password)
+    main(options.hosts, options.operation, options.user,
+         options.password)
